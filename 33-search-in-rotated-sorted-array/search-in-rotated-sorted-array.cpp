310920bbbeb5ae1a8563d490c4e438daf310622a
Rishikesh Dwivedi <rishikeshdwivedi99@gmail.com>
2025-03-04 20:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.11%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         int st = 0;
         int end = n - 1;
-        int mid = 0;
+        
         while(st <= end) {
-            mid = st + (end - st) / 2;
-            if(nums[mid] == target) {
-                return mid;
-            }
-            else if(nums[mid] >= nums[0]) {
-                if(nums[mid] >= target && nums[st] <= target) {
+            int mid = st + (end-st) / 2;
+
+            if(nums[mid]==target) return mid;
+
+            else if(nums[st]<=nums[mid]) {
+                if(nums[st]<=target && target<=nums[mid]) {
                     end = mid - 1;
                 }
                 else{
@@ -19,11 +19,11 @@ public:
                 }
             }
             else{
-                if(nums[mid] <= target && nums[end] >= target) {
-                    st = mid+1;
+                if(nums[mid]<=target && target<=nums[end]) {
+                    st = mid + 1; 
                 }
                 else{
-                    end = mid - 1;
+                    end = mid - 1; 
                 }
             }
         }
