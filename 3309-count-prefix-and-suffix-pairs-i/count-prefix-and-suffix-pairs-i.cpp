3f6097323dabd267417777d106e879ac9253aedc
Rishikesh Dwivedi <rishikeshdwivedi99@gmail.com>
2025-03-27 20:40:08 +0530

    Time: 4 ms (50.17%) | Memory: 23.5 MB (60.00%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -4,7 +4,6 @@ public:
         int n = words.size();
         int count = 0;
 
-        // Iterate over all pairs (i, j) where i < j
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
                 string a = words[i];
@@ -12,7 +11,6 @@ public:
                 int n1 = a.length();
                 int n2 = b.length();
 
-                // Check if 'a' is both a prefix and a suffix of 'b'
                 if (n1 <= n2 && b.substr(0, n1) == a && b.substr(n2 - n1, n1) == a) {
                     count++;
                 }
