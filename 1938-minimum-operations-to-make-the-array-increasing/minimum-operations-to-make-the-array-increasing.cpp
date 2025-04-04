e2b850520b44915a53a787d81beb200a6aa0fea4
Rishikesh Dwivedi <rishikeshdwivedi99@gmail.com>
2025-04-05 03:18:04 +0530

    Time: 8 ms (75.79%) | Memory: 19.4 MB (85.21%) - LeetSync

---
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                int increase = nums[i - 1] - nums[i] + 1;
+                ans += increase;
+                nums[i] += increase;
+            }
+        }
+        return ans;
+    }
+};
