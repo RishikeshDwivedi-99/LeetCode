6e9cd8fc9d5d13ee3fbaa789a463f70053318b76
Rishikesh Dwivedi <rishikeshdwivedi99@gmail.com>
2025-07-18 19:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.41%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> v;
-        int left = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
         int top = 0;
-        int right = matrix[0].size()-1;
-        int bottom = matrix.size()-1;
-
-        int direction = 0;
-
-        while(left<=right && top<=bottom) {
-            if(direction == 0) {
-                for(int col = left; col <= right; col++) {
-                    v.push_back(matrix[top][col]);
-                }
-                top++;
+        int bottom = n-1;
+        int left = 0;
+        int right = m - 1;
+        vector<int> ans;
+        while(top <= bottom && left <= right) {
+            for(int i=left;i<=right;i++) {
+                ans.push_back(matrix[top][i]);
             }
-            else if(direction == 1) {
-                for(int row = top; row<=bottom; row++) {
-                    v.push_back(matrix[row][right]);
-                }
-                right--;
+            top++;
+            for(int i=top;i<=bottom;i++) {
+                ans.push_back(matrix[i][right]);
             }
-            else if(direction == 2) {
-                for(int col = right; col >= left;col--) {
-                    v.push_back(matrix[bottom][col]);
+            right--;
+            if(top<=bottom) {
+                for(int i=right;i>=left;i--) {
+                    ans.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            else{
-                for(int row = bottom; row >= top;row--) {
-                    v.push_back(matrix[row][left]);
+            if(left<=right) {
+                for(int i=bottom;i>=top;i--) {
+                    ans.push_back(matrix[i][left]);
                 }
                 left++;
             }
-            direction = (direction+1) % 4;
         }
-        return v;
-    }
+        return ans;
+    } 
 };
